061993b6036f21e214e48f78f87c20612884c911
Egor <egor18mochalov@yandex.ru>
2023-03-21 03:46:17 +0300

    Three page

--- src/scripts/parts/slidersMain/slidersMain.js
+++ src/scripts/parts/slidersMain/slidersMain.js
@@ -1,66 +1,89 @@
 export const slidersMain = () => {
-  const swiper = new Swiper('.js-keys-carousel', {
-    // Optional parameters
-    slidesPerView: 'auto',
-    centeredSlides: true,
-    centeredSlidesBounds: true,
-    spaceBetween: 30,
-    initialSlide: 1,
-    // Navigation arrows
-    navigation: {
-      nextEl: '.js-keys__swiper-next',
-      prevEl: '.js-keys__swiper-prev',
-    }
-  });
-  const swiper2 = new Swiper('.js-keys__partners', {
-    // Optional parameters
-    slidesPerView: 'auto',
-    spaceBetween: 40,
-    breakpoints:{
-      667:{
-        spaceBetween: 84,
-      }
-    }
-  });
-  const swiper4 = new Swiper('.experience__swiper', {
-    // Optional parameters
-    slidesPerView: 'auto',
-    spaceBetween: 30,
-    breakpoints:{
-      1024:{
-        slidesPerView: 6,
-      }
-    }
-  });
-  const swiper3 = new Swiper('.js-info-swiper', {
-    // Optional parameters
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.js-info-pagination',
-    },
-    spaceBetween: 20,
-    autoHeight:true,
-    breakpoints:{
-      720:{
-        slidesPerView: 'auto',
-        spaceBetween: 30,
-      }
-    }
-  });
 
-  const swiper5 = new Swiper('.js-section-designing__swiper', {
-    // Optional parameters
-    slidesPerView: 1,
-    spaceBetween: 20,
-    breakpoints:{
-      667:{
-        slidesPerView: 'auto',
-        enabled: false,
+  document.querySelectorAll(".js-keys-carousel").forEach(e => {
+    new Swiper(e, {
+      // Optional parameters
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      centeredSlidesBounds: true,
+      spaceBetween: 30,
+      initialSlide: 1,
+      // Navigation arrows
+      navigation: {
+        nextEl: '.js-keys__swiper-next',
+        prevEl: '.js-keys__swiper-prev',
       }
-    },
-    navigation: {
-      nextEl: '.js-designing__swiper-next',
-      prevEl: '.js-designing__swiper-prev',
-    }
-  });
+    });
+  })
+
+
+  document.querySelectorAll(".js-keys__partners").forEach(e => {
+    new Swiper(e, {
+      // Optional parameters
+      slidesPerView: 'auto',
+      spaceBetween: 40,
+      breakpoints: {
+        667: {
+          spaceBetween: 84,
+        }
+      }
+    });
+  })
+
+  document.querySelectorAll(".experience__swiper").forEach(e => {
+    new Swiper(e, {
+      // Optional parameters
+      slidesPerView: 'auto',
+      spaceBetween: 30,
+      breakpoints: {
+        1024: {
+          slidesPerView: 6,
+        }
+      }
+    });
+  })
+
+  document.querySelectorAll(".expertis__swiper").forEach(e => {
+    new Swiper(e, {
+      // Optional parameters
+      slidesPerView: 'auto',
+      spaceBetween: 30,
+    });
+  })
+
+  document.querySelectorAll(".js-info-swiper").forEach(e => {
+    new Swiper(e, {
+      // Optional parameters
+      slidesPerView: 'auto',
+      pagination: {
+        el: '.js-info-pagination',
+      },
+      spaceBetween: 20,
+      autoHeight: true,
+      breakpoints: {
+        720: {
+          slidesPerView: 'auto',
+          spaceBetween: 30,
+        }
+      }
+    });
+  })
+
+  document.querySelectorAll(".js-section-designing__swiper").forEach(e => {
+    new Swiper(e, {
+      // Optional parameters
+      slidesPerView: 1,
+      spaceBetween: 20,
+      breakpoints: {
+        667: {
+          slidesPerView: 'auto',
+          enabled: false,
+        }
+      },
+      navigation: {
+        nextEl: '.js-designing__swiper-next',
+        prevEl: '.js-designing__swiper-prev',
+      }
+    });
+  })
 }
