05e34ce200f756a3f2a5aedeacd0b41f3e51078e
Egor <egor18mochalov@yandex.ru>
2023-03-22 02:38:55 +0300

    add page

--- src/scripts/parts/slidersMain/slidersMain.js
+++ src/scripts/parts/slidersMain/slidersMain.js
@@ -23,7 +23,7 @@ export const slidersMain = () => {
       slidesPerView: 'auto',
       spaceBetween: 40,
       breakpoints: {
-        667: {
+        700: {
           spaceBetween: 84,
         }
       }
@@ -36,7 +36,7 @@ export const slidersMain = () => {
       slidesPerView: 'auto',
       spaceBetween: 30,
       breakpoints: {
-        1024: {
+        1210: {
           slidesPerView: 6,
         }
       }
@@ -61,7 +61,7 @@ export const slidersMain = () => {
       spaceBetween: 20,
       autoHeight: true,
       breakpoints: {
-        720: {
+        768: {
           slidesPerView: 'auto',
           spaceBetween: 30,
         }
@@ -75,7 +75,7 @@ export const slidersMain = () => {
       slidesPerView: 1,
       spaceBetween: 20,
       breakpoints: {
-        667: {
+        700: {
           slidesPerView: 'auto',
           enabled: false,
         }
@@ -86,48 +86,4 @@ export const slidersMain = () => {
       }
     });
   })
-
-  // document.querySelectorAll(".js-list__swiper").forEach(e => {
-  //   new Swiper(e, {
-  //     // Optional parameters
-  //     slidesPerView: 1,
-  //     slidesPerGroup: 1,
-  //     spaceBetween: 20,
-  //     grid: {
-  //       rows: 3,
-  //       fill: "row",
-  //     },
-  //     breakpoints: {
-  //       1024: {
-  //         slidesPerView: 3,
-  //         slidesPerGroup: 3,
-  //         spaceBetween: 30,
-  //         grid: {
-  //           rows: 3,
-  //           fill: "row",
-  //         },
-  //       },
-  //       667: {
-  //         slidesPerView: 2,
-  //         slidesPerGroup: 2,
-  //         spaceBetween: 20,
-  //         grid: {
-  //           rows: 3,
-  //           fill: "row",
-  //         },
-  //       }
-  //     },
-  //     navigation: {
-  //       nextEl: '.js-blog__swiper-next',
-  //       prevEl: '.js-blog__swiper-prev',
-  //     },
-  //     pagination: {
-  //       el: '.section-blog__pagination',
-  //       clickable: true,
-  //       renderBullet: function (index, className) {
-  //         return `<span class="blog__swiper-pagination-bullet swiper-pagination-bullet">${index}</span>`;
-  //       },
-  //     },
-  //   });
-  // })
 }
