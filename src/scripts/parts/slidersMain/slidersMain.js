f836d5f039d435e8b86b5bef87f36ece4c8aec41
Egor <egor18mochalov@yandex.ru>
2023-03-21 17:22:53 +0300

    Blog page

--- src/scripts/parts/slidersMain/slidersMain.js
+++ src/scripts/parts/slidersMain/slidersMain.js
@@ -86,4 +86,48 @@ export const slidersMain = () => {
       }
     });
   })
+
+  document.querySelectorAll(".js-list__swiper").forEach(e => {
+    new Swiper(e, {
+      // Optional parameters
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 20,
+      grid: {
+        rows: 3,
+        fill: "row",
+      },
+      breakpoints: {
+        1024: {
+          slidesPerView: 3,
+          slidesPerGroup: 3,
+          spaceBetween: 30,
+          grid: {
+            rows: 3,
+            fill: "row",
+          },
+        },
+        667: {
+          slidesPerView: 2,
+          slidesPerGroup: 2,
+          spaceBetween: 20,
+          grid: {
+            rows: 3,
+            fill: "row",
+          },
+        }
+      },
+      navigation: {
+        nextEl: '.js-blog__swiper-next',
+        prevEl: '.js-blog__swiper-prev',
+      },
+      pagination: {
+        el: '.section-blog__pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+          return `<span class="blog__swiper-pagination-bullet swiper-pagination-bullet">${index}</span>`;
+        },
+      },
+    });
+  })
 }
